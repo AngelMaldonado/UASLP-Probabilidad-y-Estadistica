e57f5525ead12ef4bd1676f84d97489cd05f99d4
AngelMaldonado <amaldonadojuarez@gmail.com>
2022-11-05 08:37:55 -0600

    🧠MSC - Loading data from csv and txt

---
+++ MarinStatsCourse/04LoadingData/04LoadingData.R
@@ -0,0 +1,12 @@
+### Importing csv data file ###
+data1 <- read.csv(file.choose(), header = TRUE)
+data1
+
+data2 <- read.table(file.choose(), header = TRUE, sep = ",")
+data2
+
+### Importing txt data file
+data3 <- read.delim(file.choose(), header = T)
+data3
+
+data4 <- read.table(file.choose(), header = T, sep = "\t")
