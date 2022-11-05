8291662830ff5666ee220a477db951fe2cfe78dd
AngelMaldonado <amaldonadojuarez@gmail.com>
2022-11-05 07:42:02 -0600

    🧠MSC - Basic Aritmetic

---
+++ MarinStatsCourse/01BasicAritmetic.R
@@ -0,0 +1,44 @@
+### Variable assignation ###
+x = 11
+x
+
+y <- 7
+y
+
+rm(y) # Remove a variable from environment
+
+x.1 <- 14
+x.1
+
+1x <- 22 # Error
+
+xx <- "marin"
+xx
+
+yy <- "1"
+yy
+
+### Operations ###
+11 + 14
+7 * 9
+x + y
+
+z <- x + y
+
+x - y
+
+x / y
+
+x * y
+
+y^2
+
+sqrt(y)
+
+log(y)
+
+exp(y)
+
+log2(y)
+
+abs(-14)
